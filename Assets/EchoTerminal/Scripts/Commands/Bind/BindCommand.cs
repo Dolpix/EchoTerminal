e7067f00fcc1a461dbc2a5c2f6cfc03bc1275450
Dolpix <kikigoldwolf@gmail.com>
2026-03-18 23:38:43 +0000

    Refactor: Binds are renamed

--- Assets/EchoTerminal/Scripts/Commands/Bind/BindCommand.cs
+++ Assets/EchoTerminal/Scripts/Commands/Bind/BindCommand.cs
@@ -6,21 +6,28 @@ namespace EchoTerminal
 {
 public static class BindCommand
 {
-	[TerminalCommand("bind", "Bind a key to a command")]
-	private static void Bind(Key key, string command)
+	[TerminalCommand("BindsAdd", "Bind a key to a command")]
+	private static void BindsAdd(Key key, string command)
 	{
 		BindStore.Set(key, command);
 		Debug.Log($"Bound {key} to: {command}");
 	}
 
-	[TerminalCommand("unbind", "Remove a key binding")]
-	private static void Unbind(Key key)
+	[TerminalCommand("BindsRemove", "Remove a key binding")]
+	private static void BindsRemove(Key key)
 	{
 		Debug.Log(BindStore.Remove(key) ? $"Unbound {key}" : $"No bind found for '{key}'");
 	}
 
-	[TerminalCommand("binds", "List all current key bindings")]
-	private static void Binds()
+	[TerminalCommand("BindsClear", "Remove all key bindings")]
+	private static void BindsClear()
+	{
+		BindStore.Clear();
+		Debug.Log("All binds removed.");
+	}
+
+	[TerminalCommand("BindsLog", "List all current key bindings")]
+	private static void BindsLog()
 	{
 		var all = BindStore.GetAll();
 
